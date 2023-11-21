6709aec048d331896e364035c0eef20fa1f21b65
manojkumarkandakatla <manojkumarkandakatla1324@gmail.com>
2023-11-21 16:21:04 +0530

    Add project files.

---
+++ Controllers/SearchController .cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kme.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Topic()
+        {
+            return View();
+        }
+
+        public ActionResult AdminPage()
+        {
+            return View();
+
+        }
+    }
+}
